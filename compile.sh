420fa0f1b731d63aadb05049e4fe68556bd6c15f
PNone <urikita8@gmail.com>
2024-07-04 14:12:04 +0300

    initial version

---
+++ compile.sh
@@ -0,0 +1,1 @@
+g++ -DNDEBUG -std=c++17 -Wall -pedantic-errors -Werror -g -o agamBhbt *.cpp
---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main() {
+    cout << "       _.-^^---....,,--       " << endl;
+    cout << "   _--                  --_   " << endl;
+    cout << "  <                        >) " << endl;
+    cout << "  |                         | " << endl;
+    cout << "   \\._                   _./  " << endl;
+    cout << "      ```--. . , ; .--'''     " << endl;
+    cout << "            | |   |           " << endl;
+    cout << "         .-=||  | |=-.        " << endl;
+    cout << "         `-=#$%&%$#=-'        " << endl;
+    cout << "            | ;  :|           " << endl;
+    cout << "   _____.,-#%&$@%#&#~,._____  " << endl;
+    cout << "                             " << endl;
+    return 0;
+}
